65391f96e0243a6d3edba560a00a284bdb725ff8
Kyle Luke <th.oldschool@gmail.com>
2019-10-22 05:04:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,7 +38,16 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+  SELECT series.title
+  FROM series 
+  JOIN books ON books.series_id = series.id
+  JOIN character_books ON character_books.book_id = books.id
+  JOIN characters ON characters.id = character_books.character_id
+  GROUP BY characters.species HAVING species IS 'human'
+  ORDER BY COUNT(*)
+  DESC LIMIT 1
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
