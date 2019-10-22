a00a4f155f5aa15f30c237a4ebfe54a434d072cf
Kyle Luke <th.oldschool@gmail.com>
2019-10-22 04:50:33 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -28,7 +28,13 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "
+  SELECT DISTINCT characters.species, COUNT(characters.species)
+  FROM characters
+  GROUP BY characters.species
+  ORDER BY characters.species
+  DESC LIMIT 1
+  "
 end
 
 def select_series_title_with_most_human_characters
