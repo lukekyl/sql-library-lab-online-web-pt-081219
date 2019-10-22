b00271ed717e710ddcd893e60eed438742b2c072
Kyle Luke <th.oldschool@gmail.com>
2019-10-22 04:36:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "
   SELECT books.title, books.year
   FROM books
-  JOIN series ON series.id = books.series_id
+  WHERE books.series_id = 1
+  GROUP BY books.title
+  ORDER BY books.year
   "
 end
 
